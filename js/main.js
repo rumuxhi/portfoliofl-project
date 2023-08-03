56d42ca5b89f4365ddb24514b582c3d2cb5951c0
rumuxhi <garmon.04@mail.ru>
2023-08-03 20:55:09 +0300

    added dark mode, fixed bug on main page when dark mode is enabled

--- js/main.js
+++ js/main.js
@@ -1,6 +1,47 @@
 const btnDarkMode = document.querySelector(".dark-mode-btn");
 
+// Checking dark mode in user system settings
+if (
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches
+) {
+  btnDarkMode.classList.add("dark-mode-btn--active");
+  document.body.classList.add("dark");
+}
+
+// Checking dark mode in local storage
+if (localStorage.getItem("darkMode") === "dark") {
+  btnDarkMode.classList.add("dark-mode-btn--active");
+  document.body.classList.add("dark");
+} else if (localStorage.getItem("darkMode") === "light") {
+  btnDarkMode.classList.remove("dark-mode-btn--active");
+  document.body.classList.remove("dark");
+}
+
+// If user system settings was changed
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", () => {
+    const newColorScheme = event.matches ? "dark" : "light";
+
+    if (newColorScheme === "dark") {
+      btnDarkMode.classList.add("dark-mode-btn--active");
+      document.body.classList.remove("dark");
+      localStorage.setItem("darkMode", "dark");
+    } else {
+      btnDarkMode.classList.remove("dark-mode-btn--active");
+      document.body.classList.remove("dark");
+      localStorage.setItem("darkMode", "light");
+    }
+  });
+// Enabling dark mode when button clicked
 btnDarkMode.onclick = function () {
   btnDarkMode.classList.toggle("dark-mode-btn--active");
-  document.body.classList.toggle("dark");
+  const isDark = document.body.classList.toggle("dark");
+
+  if (isDark) {
+    localStorage.setItem("darkMode", "dark");
+  } else {
+    localStorage.setItem("darkMode", "light");
+  }
 };
